789ebb6c6723a50f258f955a6d530f7c758f16f4
Thiago Oliveira <thiago.oliveira@kestraa.com>
2020-05-13 18:13:37 -0300

    finished module4

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input type="text" name="search" placeholder="Search Books" v-model="searchInput" />
     <hr />
     <h2>Filtered Books By Ownership</h2>
     <ul>
-      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
+      <book-item v-for="book in searchedBooks" :key="book.id" :book="book"></book-item>
     </ul>
     <select name="holding" v-model="holding">
       <option v-for="filter in filters" :key="filter">{{ filter }}</option>
@@ -50,7 +51,8 @@ export default {
         }
       ],
       filters: ["bought", "borrowed"],
-      holding: "bought"
+      holding: "bought",
+      searchInput: ""
     };
   },
   components: {
@@ -60,6 +62,12 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
